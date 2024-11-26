83970f6b8784927205db3e55304fa1376bd83bd3
teinsisu <itstore.ungaran@gmail.com>
2024-11-27 02:49:11 +0700

    menambah table danin

---
+++ database/migrations/2024_11_26_193808_create_danins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('danins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('bagian')->nullable();
+            $table->string('gambar')->nullable();
+            $table->string('link')->nullable();
+            $table->longText('kode')->nullable();
+            $table->longText('informasi')->nullable();
+            $table->boolean('aktif')->default('false');
+            $table->integer('posisi')->nullable();
+            $table->unsignedBigInteger('danin_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('danins');
+    }
+};
---
+++ resources/views/admin/pages.blade.php
@@ -0,0 +1,5 @@
+<x-layout-main>
+
+    @livewire('admin.dashboard')
+
+</x-layout-main>
